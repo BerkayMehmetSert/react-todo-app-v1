ad1c02cbdfaebd93dc9d97bcc9b09b3f16269771
BerkayMehmetSert <berkaymehmetsert@gmail.com>
2022-03-19 14:11:02 +0300

    Project completed.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = todo => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    // add the todo to the list
+    setList([...list, newTodo]);
+    //clear the input
+    setInput('');
+  };
+  const deleteTodo = id => {
+    // filter out the todo with the id
+    const newList = list.filter(todo => todo.id !== id);
+    setList(newList);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type='text'
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map(todo => (
+          <li key={todo.id} style={{ padding: '6px' }}>
+            {todo.todo}
+            <button
+              onClick={() => deleteTodo(todo.id)}
+              style={{ marginLeft: '6px' }}
+            >
+              &times;
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
